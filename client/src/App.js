183b00b6ac95389375d6137787b7380110aae913
Quincy Iheme <qiheme@gmail.com>
2020-09-08 12:56:27 -0400

    Add router and move home page to its own component

--- client/src/App.js
+++ client/src/App.js
@@ -1,90 +1,35 @@
-import React, { useEffect, useState } from "react";
-import Card from "./Card";
-import StockModal from "./StockModal";
-import { Button, CardColumns, Col, Container, Form, Jumbotron, Row } from "react-bootstrap";
+import React from "react";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import News from "./News";
+import { Nav } from "react-bootstrap";
 
-import axios from "axios";
-import "./App.css";
-
-function App() {
-  const [stocks, setStocks] = useState([]);
-  const [searchedStock, setSearchedStock] = useState({});
-  const [modalShow, setModalShow] = useState(false);
-
-  const searchNews = () => {
-    axios
-      .get(`/api/finnhub/general`)
-      .then((response) => {
-        console.log(response);
-        setStocks(response.data);
-      });
-  };
-
-  const searchStock = () => {
-    const symbol = document.getElementById("stock-input").value;
-    axios
-      .get(`/api/finnhub/quote/${symbol}`)
-      .then((response) => {
-        console.log(response);
-        response.data.symbol = symbol;
-        setSearchedStock(response.data);
-        setModalShow(true);
-      });
-  };
-
-  useEffect(searchNews, []);
+export default function App() {
   return (
-    <div className="App">
-      <Container className="container">
-        <Row className="stock-row">
-          <Col>
-            <Jumbotron>
-              <h1>Hello, world!</h1>
-              <p>This is a very simple application to search stock news.</p>
-              <Form.Group>
-                <Form.Row>
-                  <Col>
-                    <Form.Control
-                      id="stock-input"
-                      size="sm"
-                      type="text"
-                      placeholder="Please enter a stock"
-                    />
-                  </Col>
-                </Form.Row>
-              </Form.Group>
-              <p>
-                <Button type="submit" onClick={searchStock} variant="primary">
-                  Search
-                </Button>
-              </p>
-            </Jumbotron>
-            <br></br>
-            <CardColumns>
-              {stocks.map((stock) => {
-                return (
-                  <Col>
-                    <Card
-                      img={stock.image}
-                      link={stock.url}
-                      title={stock.headline}
-                      text={stock.summary}
-                    ></Card>
-                  </Col>
-                );
-              })}
-            </CardColumns>
-          </Col>
-        </Row>
-      </Container>
-      <StockModal
-        show={modalShow}
-        stock={searchedStock}
-        onHide={() => setModalShow(false)}
-      />
+    <Router>
+      <div>
+        <Nav
+          activeKey="/"
+          onSelect={(selectedKey) => alert(`selected ${selectedKey}`)}
+        >
+          <Nav.Item>
+            <Nav.Link href="/">Home</Nav.Link>
+          </Nav.Item>
+        </Nav>
 
-    </div>
+        {/* A <Switch> looks through its children <Route>s and
+            renders the first one that matches the current URL. */}
+        <Switch>
+          {/* <Route path="/about">
+            <About />
+          </Route>
+          <Route path="/users">
+            <Users />
+          </Route> */}
+          <Route path="/">
+            <News />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
-
-export default App;
---
+++ client/src/News.js
@@ -0,0 +1,91 @@
+import React, { useEffect, useState } from "react";
+import Card from "./Card";
+import StockModal from "./StockModal";
+import { Button, CardColumns, Col, Container, Form, Jumbotron, Row } from "react-bootstrap";
+
+import axios from "axios";
+import "./News.css";
+
+function News() {
+  const [stocks, setStocks] = useState([]);
+  const [searchedStock, setSearchedStock] = useState({});
+  const [modalShow, setModalShow] = useState(false);
+
+  const searchNews = () => {
+    axios
+      .get(`/api/finnhub/general`)
+      .then((response) => {
+        console.log(response);
+        setStocks(response.data);
+      });
+  };
+
+  const searchStock = () => {
+    const symbol = document.getElementById("stock-input").value;
+    axios
+      .get(`/api/finnhub/quote/${symbol}`)
+      .then((response) => {
+        console.log(response);
+        response.data.symbol = symbol;
+        setSearchedStock(response.data);
+        setModalShow(true);
+      });
+  };
+
+  useEffect(searchNews, []);
+
+  return (
+    <div className="News">
+      <Container className="container">
+        <Row className="stock-row">
+          <Col>
+            <Jumbotron>
+              <h1>Hello, world!</h1>
+              <p>This is a very simple Newslication to search stock news.</p>
+              <Form.Group>
+                <Form.Row>
+                  <Col>
+                    <Form.Control
+                      id="stock-input"
+                      size="sm"
+                      type="text"
+                      placeholder="Please enter a stock"
+                    />
+                  </Col>
+                </Form.Row>
+              </Form.Group>
+              <p>
+                <Button type="submit" onClick={searchStock} variant="primary">
+                  Search
+                </Button>
+              </p>
+            </Jumbotron>
+            <br></br>
+            <CardColumns>
+              {stocks.map((stock) => {
+                return (
+                  <Col>
+                    <Card
+                      img={stock.image}
+                      link={stock.url}
+                      title={stock.headline}
+                      text={stock.summary}
+                    ></Card>
+                  </Col>
+                );
+              })}
+            </CardColumns>
+          </Col>
+        </Row>
+      </Container>
+      <StockModal
+        show={modalShow}
+        stock={searchedStock}
+        onHide={() => setModalShow(false)}
+      />
+
+    </div>
+  );
+}
+
+export default News;
