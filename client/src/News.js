9b992a79db50d2b46407c8d7e32ced41185ef408
Quincy Iheme <qiheme@gmail.com>
2020-09-08 14:46:32 -0400

    Add loading spinner in home

--- client/src/News.js
+++ client/src/News.js
@@ -1,15 +1,16 @@
 import React, { useEffect, useState } from "react";
 import Card from "./Card";
 import StockModal from "./StockModal";
-import { Button, CardColumns, Col, Container, Form, Jumbotron, Row } from "react-bootstrap";
+import { Button, CardColumns, Col, Container, Form, Jumbotron, Row, Spinner } from "react-bootstrap";
 
 import axios from "axios";
 import "./News.css";
 
 function News() {
-  const [stocks, setStocks] = useState([]);
-  const [searchedStock, setSearchedStock] = useState({});
+  const [loading, setLoading] = useState(true);
   const [modalShow, setModalShow] = useState(false);
+  const [searchedStock, setSearchedStock] = useState({});
+  const [stocks, setStocks] = useState([]);
 
   const searchNews = () => {
     axios
@@ -17,6 +18,7 @@ function News() {
       .then((response) => {
         console.log(response);
         setStocks(response.data);
+        setLoading(false);
       });
   };
 
@@ -32,60 +34,85 @@ function News() {
       });
   };
 
+  const loaderStyles = {
+    minHeight: "100vh",
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+    justifyContent: "center"
+  };
+
   useEffect(searchNews, []);
 
-  return (
-    <div className="News">
-      <Container className="container">
-        <Row className="stock-row">
-          <Col>
-            <Jumbotron>
-              <h1>Hello, world!</h1>
-              <p>This is a very simple Newslication to search stock news.</p>
-              <Form.Group>
-                <Form.Row>
-                  <Col>
-                    <Form.Control
-                      id="stock-input"
-                      size="sm"
-                      type="text"
-                      placeholder="Please enter a stock"
-                    />
-                  </Col>
-                </Form.Row>
-              </Form.Group>
-              <p>
-                <Button type="submit" onClick={searchStock} variant="primary">
-                  Search
-                </Button>
-              </p>
-            </Jumbotron>
-            <br></br>
-            <CardColumns>
-              {stocks.map((stock) => {
-                return (
-                  <Col>
-                    <Card
-                      img={stock.image}
-                      link={stock.url}
-                      title={stock.headline}
-                      text={stock.summary}
-                    ></Card>
-                  </Col>
-                );
-              })}
-            </CardColumns>
-          </Col>
-        </Row>
-      </Container>
-      <StockModal
-        show={modalShow}
-        stock={searchedStock}
-        onHide={() => setModalShow(false)}
-      />
+  if (loading) {
+    return (
+      <div style={loaderStyles}>
+        <Container>
+          <Row style={{textAlign: `center`}}>
+            <Col>
+              <Spinner animation="border" role="status">
+                <span className="sr-only">Loading...</span>
+              </Spinner>
+            </Col>
+          </Row>
+        </Container>
+      </div>
+    );
+  } else {
+    return (
+      <div className="News">
+        <Container className="container">
+          <Row className="stock-row">
+            <Col>
+              <Jumbotron>
+                <h1>Hello, world!</h1>
+                <p>This is a very simple Newslication to search stock news.</p>
+                <Form.Group>
+                  <Form.Row>
+                    <Col>
+                      <Form.Control
+                        id="stock-input"
+                        size="sm"
+                        type="text"
+                        placeholder="Please enter a stock"
+                      />
+                    </Col>
+                  </Form.Row>
+                </Form.Group>
+                <p>
+                  <Button type="submit" onClick={searchStock} variant="primary">
+                    Search
+                  </Button>
+                </p>
+              </Jumbotron>
+              <br></br>
+              <CardColumns>
+                {stocks.map((stock) => {
+                  return (
+                    <Col>
+                      <Card
+                        img={stock.image}
+                        link={stock.url}
+                        title={stock.headline}
+                        text={stock.summary}
+                      ></Card>
+                    </Col>
+                  );
+                })}
+              </CardColumns>
+            </Col>
+          </Row>
+        </Container>
+        <StockModal
+          show={modalShow}
+          stock={searchedStock}
+          onHide={() => setModalShow(false)}
+        />
+  
+      </div>
+    );
+  }
 
-    </div>
-  );
 }
 
 export default News;
